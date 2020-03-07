1e43f72ba627398b0e740671934b83ebf30d9922
jorgeduardorobles@outlook.com <jorgeduardorobles@outlook.com>
2020-03-07 10:11:24 -0400

    Primer UnitTest ocn Api

---
+++ webapiCalculadora/UnitTestCalculadora/UnitTestCalculadora.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using webapiCalculadora.Controllers;
+
+namespace UnitTestCalculadora
+{
+    [TestClass]
+    public class UnitTestCalculadora
+    {
+        [TestMethod]
+        public void TestAddGet1()
+        {
+            //Arrange
+            CalculatorController Calc = new CalculatorController();
+            int numa = 3;
+            int numb = 4;
+            int numres = 7;
+
+            //Act = Ejecución
+            int result = Calc.Add(numa, numb);
+            //Assert = Verificación
+            Assert.AreEqual(numres, result);
+
+        }
+    }
+}
